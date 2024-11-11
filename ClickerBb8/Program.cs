88e9b04aefda48900de571f934d7b7f9222172a2
kenriortega <kenriortega@gmail.com>
2024-11-11 12:14:37 -0800

    ref: rollback to 0 code boilerplate

--- ClickerBb8/Program.cs
+++ ClickerBb8/Program.cs
@@ -5,30 +5,16 @@ using Share;
 
 var builder = Host.CreateApplicationBuilder(args);
 builder.Services.AddHostedService<Worker>();
+builder.Services.Configure<KafkaOptions>(builder.Configuration.GetSection("Kafka"));
+builder.Services.AddSingleton<IDBConnectionFactory>(_ =>
+    new NpgsqlDbConnectionFactory(builder.Configuration["Database:ConnectionString"]!));
+
 
 foreach (var c in builder.Configuration.AsEnumerable())
 {
     Console.WriteLine(c.Key + " = " + c.Value);
 }
 
-
-IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices((hostContext,services )=>
-    {
-        var configuration = new ConfigurationBuilder()
-            .SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("appsettings.json")
-            .AddJsonFile($"appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json", optional: true,
-                reloadOnChange: true)
-            .AddEnvironmentVariables()
-            .Build();
-
-        services.Configure<KafkaOptions>(
-            configuration.GetSection("Kafka"));
-        services.AddSingleton<IDBConnectionFactory>(_ =>
-            new NpgsqlDbConnectionFactory(builder.Configuration["Database:ConnectionString"]!));;
-    })
-    .Build();
-
+var host = builder.Build();
 
 host.Run();
